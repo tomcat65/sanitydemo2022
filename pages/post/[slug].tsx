0691cff33cef8639557accf413ed52bbfd301870
TomCat65 <talvarez.uh@gmail.com>
2022-02-20 06:05:48 -0600

    params slug working

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,32 +1,57 @@
-import {GetStaticProps} from 'next';
-import Header from "../../components/Header";
-import { sanityClient,urlFor } from "../../sanity";
-import { Post } from "../../typings";
+import { GetStaticProps } from 'next'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
 
-export default function Post() {
-    return (
-        <main>
-            <Header/>
-        </main>
-    );
+interface Props {
+  post: Post
 }
-export const getStaticPaths=async ()=>{
-const query=`*[_type=="post]{
+function Post({ post }: Props) {
+  console.log(post)
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post" ]{
     _id,
-    slug{
-        current
-    }
-}`;
-const posts=await sanityClient.fetch(query)
-const paths=posts.map((post:Post)=>({
-    params:post.slug.current
-}))
-return {
+    slug{current},
+  }`
+  const posts = await sanityClient.fetch(query);
+  const paths = posts.map((post: Post) => ({
+    params: {slug:post.slug.current}
+  }))
+  return {
     paths,
-    fallback:"blocking"
-}
+    fallback: 'blocking',
+  };
 };
-export const getStaticProps: GetStaticProps =async ({params})=>{
 
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    createdAt,
+    title,
+    author->{
+        name,
+        image
+    },
+    description,
+    mainImage,
+    slug,
+    body}`
+  const post = await sanityClient.fetch(query, { slug: params?.slug })
+  if (!post) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      post,
+    },
+  }
 }
-  
