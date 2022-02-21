00970b2437846629799378900935d0b46b8b9500
TomCat65 <talvarez.uh@gmail.com>
2022-02-21 00:05:37 -0600

    build is almost complete, comments functionality working

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,48 +3,177 @@ import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
 
 interface Props {
   post: Post
 }
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 function Post({ post }: Props) {
+  const [submitted, setSubmitted] = useState(false)
   console.log(post)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+        setSubmitted(true)
+      })
+      .catch((err) => {
+        console.log(err)
+        setSubmitted(false)
+      })
+  }
+
   return (
-    <main >
+    <main>
       <Header />
-      <img 
-      className='w-full h-40 md:h-72 object-cover '
-      src={urlFor(post.mainImage).url()!} alt="post img" />
-      <article className='max-w-3xl mx-auto p-5'>
-          <h1 className='text-3xl mt-10 mb-3 '>{post.title}</h1>
-          <h2 className='text-xl font-light text-gray-500 mb-2'>{post.description}</h2>
-          <div className='flex items-center space-x-2'>
-              <img 
-              className='h-10 w-10 rounded-full'
-              src={urlFor(post.author.image).url()!} alt="" />
-              <p className='text-sm font-extralight'>Blog post by <span className='text-green-600'>{post.author.name}</span>  - Published at{" "}{new Date(post._createdAt).toLocaleString()}</p>
-          </div>
-          <div className='mt-10'>
-              <PortableText
-              className=''
-              dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
-              projectId={process.env.NEXT_PUBLICT_SANITY_PROJECT_ID!}
-              content={post.body}
-              serializers={
-                  {
-                      h1:(props:any)=>(<h1 className='text-2xl font-bold my-5' {...props}/>),
-                      h2:(props:any)=>(<h2 className='text-xl font-bold my-5' {...props}/>),
-                      li:({children}:any)=>(<li className='ml-4 list-disc'>{children}</li>),
-                      link:({href,children}:any)=>(
-                          <a href={href} className="text-blue-500 hover:underline">{children}</a>
-                      ),
-                      image: ({asset}:any) => <img className="h-60 w-60 object-cover md:h-72 md:w-72 lg:w-max lg:h-max"
-                      src={urlFor({asset}).url()} alt=""/>
-                  }
-              }
-              />
-          </div>
+      <img
+        className="h-40 w-full object-cover md:h-72 "
+        src={urlFor(post.mainImage).url()!}
+        alt="post img"
+      />
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl ">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLICT_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+              image: ({ asset }: any) => (
+                <img
+                  className="h-60 w-60 object-cover md:h-72 md:w-72 lg:h-max lg:w-max"
+                  src={urlFor({ asset }).url()}
+                  alt=""
+                />
+              ),
+            }}
+          />
+        </div>
       </article>
+      <hr className="mx-auto mb-5 max-w-lg border border-yellow-500" />
+      {submitted ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 p-10 text-white">
+          <h3 className="text-3xl font-bold">
+            Thank you for submitting your comment!
+          </h3>
+          <p>Once its been approved it will appear below!</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="mt-2 py-3" />
+          <input
+            {...register('_id')}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+          <label className="mb-5 block">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register('name', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 hover:ring focus:ring"
+              placeholder="John Smith"
+              type="text"
+            />
+          </label>
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">email</span>
+            <input
+              {...register('email', { required: true })}
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 hover:ring focus:ring"
+              placeholder="xx@xxx.xx"
+              type="email"
+            />
+          </label>
+
+          <label className="mb-5 block">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 hover:ring focus:ring"
+              placeholder="your comments"
+              rows={8}
+            />
+          </label>
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">-The name field is required</span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">-The email field is required</span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">-Please add your comments</span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+          />
+        </form>        
+      )}
+      {/* Comments */}
+      <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
+          <h3 className='text-4xl'>Comments</h3>
+          <hr className='pb-2'/>
+          {post.comments.map((comment)=>(
+              <div  key={comment._id}>
+                  <p><span className='text-yellow-500'>{comment.name}:</span> {comment.comment}</p>
+              </div>
+          ))}
+      </div>
     </main>
   )
 }
@@ -55,15 +184,15 @@ export const getStaticPaths = async () => {
     _id,
     slug{current},
   }`
-  const posts = await sanityClient.fetch(query);
+  const posts = await sanityClient.fetch(query)
   const paths = posts.map((post: Post) => ({
-    params: {slug:post.slug.current}
+    params: { slug: post.slug.current },
   }))
   return {
     paths,
     fallback: 'blocking',
-  };
-};
+  }
+}
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const query = `*[_type == "post" && slug.current == $slug][0]{
@@ -74,6 +203,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         name,
         image
     },
+    "comments":*[_type == "comment" && post._ref == ^._id &&
+  approved == true]
+,
     description,
     mainImage,
     slug,
